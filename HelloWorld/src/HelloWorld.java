99c41f09e10b033b7c4f1d86ba35dd55ec4a67c9
comp3333 <comp3333@gmail.com>
2015-02-10 11:35:09 +0800

    Update HelloWorld.java
    delete all

--- HelloWorld/src/HelloWorld.java
+++ HelloWorld/src/HelloWorld.java
@@ -3,7 +3,7 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-        System.out.println("Hello World, Hi, all");
+        System.out.println("Hello World, Hi");
 	}
 
 }
